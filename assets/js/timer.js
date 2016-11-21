9388cbbea05d1f0e23dc49bcb3c7c81e97d41552
Todd Croak-Falen <Todd Croak-Falen>
2016-11-21 03:26:27 -0800

    Got timer to work, with basic placeholder alert instead of a modal popup.

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,1 +1,17 @@
-// Placeholder text.
+// Once jQuery is properly linked, add "on load" here.
+
+// alert("timer.js is linked!");
+
+// 30-second timer.  SEE 07-25 SIMPLE TIMER ACTIVITY.
+setTimeout(popup, 1000 * 30);
+
+// Pop-up modal inviting user to sign up for bonus content.
+function popup() {
+	// Change this alert to a modal:
+    alert("Would you like to receive free bonus content?");
+}
+
+// Check if popup() has been called this session.  AJAX??
+	// If yes, STOP.  DO NOT call popup().
+	// If no, call popup().
+		// Log that popup has been called.
