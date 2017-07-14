d8445b6abf45ce9866f51367e43aa9d53307ce9b
Todd Croak-Falen <toddcf@gmail.com>
2017-07-14 10:57:13 -0700

    Waypoints, animation fadeIns, and delays are all working properly on TESTIMONIALS.

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,19 +3,19 @@ $( document ).ready( function() {
 	$( ".js--t1" ).waypoint( function( direction ) {
 			$( ".js--t1" ).addClass( "animated-1 fadeIn" );
 		}, {
-			offset: "50%"
+			offset: "75%"
 		});
 
 	$( ".js--t2" ).waypoint( function( direction ) {
 			$( ".js--t2" ).addClass( "animated-1-and-half fadeIn" );
 		}, {
-			offset: "50%"
+			offset: "75%"
 		});
 
 	$( ".js--t3" ).waypoint( function( direction ) {
 			$( ".js--t3" ).addClass( "animated-2 fadeIn" );
 		}, {
-			offset: "50%"
+			offset: "75%"
 		});
 
 } );
