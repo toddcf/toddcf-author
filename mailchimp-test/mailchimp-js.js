66275279d30665d02c4f66703e2a6c5ec947f797
Todd Croak-Falen <toddcf@gmail.com>
2017-07-18 15:36:55 -0700

    Added HTTPS to MailChimp CSS link, and now the page loads quickly and with correct styling.

--- mailchimp-test/mailchimp-js.js
+++ mailchimp-test/mailchimp-js.js
@@ -1,4 +1,23 @@
-alert("mailchimp-js is linked.");
+$( document ).ready( function() {
+
+	( function( $ ) {
+		
+		window.fnames = new Array();
+		window.ftypes = new Array();
+		fnames[ 0 ] = 'EMAIL';
+		ftypes[ 0 ] = 'email';
+		fnames[ 1 ] = 'FNAME';
+		ftypes[ 1 ] = 'text';
+		fnames[ 2 ] = 'LNAME';
+		ftypes[ 2 ] = 'text';
+	
+	}
+
+	(jQuery));
+	
+	var $mcj = jQuery.noConflict( true );
+
+} );
 
 
-(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
+// (function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);
