b0b7f9a362ad2dc35fa19b27b91b467b1aaaab31
Todd Croak-Falen <toddcf@gmail.com>
2022-08-25 22:22:38 -0700

    Begin JS to find and trim any URLs that end with index

---
+++ assets/scripts/dynamic-links.js
@@ -0,0 +1,6 @@
+let indexLinks = Array.from(document.querySelectorAll('a'));
+const trimIndex = (str) => {
+  if (str.slice(-5 === 'index')) {
+    // Replace that final 'index' with an empty string -- but only the final 'index', in case there are multiples in the pathname on the way there.
+  }
+}
