f47f4f6056e0b0a32af8ab74a767858658e59224
Todd Croak-Falen <toddcf@gmail.com>
2022-08-25 22:31:33 -0700

    Rename function

--- assets/scripts/dynamic-links.js
+++ assets/scripts/dynamic-links.js
@@ -1,5 +1,5 @@
 let indexLinks = Array.from(document.querySelectorAll('a'));
-const trimIndex = (str) => {
+const modifyHref = (str) => {
   if (str.slice(-5 === 'index')) {
     if (!!window.location.host) {
       // If Prod or GH Pages, replace that final 'index' with an empty string -- but only the final 'index', in case there are multiples in the pathname on the way there.
