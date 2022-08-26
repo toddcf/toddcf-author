800f748b4f3c11de18020f9743b28f777da2107d
Todd Croak-Falen <toddcf@gmail.com>
2022-08-26 16:57:07 -0700

    Add dynamic link programming for local, gh-pages, and prod

--- assets/scripts/dynamic-links.js
+++ assets/scripts/dynamic-links.js
@@ -1,11 +1,33 @@
-let indexLinks = Array.from(document.querySelectorAll('a'));
-const modifyHref = (str) => {
-  if (str.slice(-5 === 'index')) {
-    if (!!window.location.host) {
-      // If Prod or GH Pages, replace that final 'index' with an empty string -- but only the final 'index', in case there are multiples in the pathname on the way there.
-    } else {
-      // If Local File:
-      str.href += '.html';
+let env = '';
+switch (window.location.host) {
+  case 'toddcf.com':
+    env = 'prod';
+    break;
+  case 'toddcf.github.io':
+    env = 'gh-pages';
+    break;
+  default:
+    env = 'local';
+}
+
+let siteLinks = Array.from(document.querySelectorAll('a'));
+const modifyHref = (siteLink) => {
+  if (!!siteLink.href.slice(-5 === 'index')) {
+    switch (env) {
+      case 'prod':
+      case 'gh-pages':
+        // Remove 'index' from the end of any hrefs.
+        siteLink.href = siteLink.href.substring(0, siteLink.href.length - 5);
+        break;
+      case 'local':
+        siteLink.href += '.html';
     }
   }
 }
+
+siteLinks.forEach(modifyHref);
+
+// USE CASES:
+// Test everything in local, gh-pages, and prod.
+// anchor tags
+// buttons
