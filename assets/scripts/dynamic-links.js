3ba65bbc002f6342032350a5799a3d942948845b
Todd Croak-Falen <toddcf@gmail.com>
2022-08-25 22:29:21 -0700

    Append .html if local file

--- assets/scripts/dynamic-links.js
+++ assets/scripts/dynamic-links.js
@@ -1,6 +1,11 @@
 let indexLinks = Array.from(document.querySelectorAll('a'));
 const trimIndex = (str) => {
   if (str.slice(-5 === 'index')) {
-    // Replace that final 'index' with an empty string -- but only the final 'index', in case there are multiples in the pathname on the way there.
+    if (!!window.location.host) {
+      // If Prod or GH Pages, replace that final 'index' with an empty string -- but only the final 'index', in case there are multiples in the pathname on the way there.
+    } else {
+      // If Local File:
+      str.href += '.html';
+    }
   }
 }
