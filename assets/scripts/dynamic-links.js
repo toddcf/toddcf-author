4eae9470224aa33b354b00a658056208256ec15c
Todd Croak-Falen <toddcf@gmail.com>
2022-08-26 18:07:04 -0700

    Remove console logs after testing

--- assets/scripts/dynamic-links.js
+++ assets/scripts/dynamic-links.js
@@ -12,23 +12,16 @@ switch (window.location.host) {
 
 let siteLinks = Array.from(document.querySelectorAll('a'));
 const modifyHref = (siteLink) => {
-  console.log('modifyHref invoked. siteLink.href.slice(-5) === ', siteLink.href.slice(-5));
   if (siteLink.href.slice(-5) === 'index') {
-    console.log('This link ends with index.');
-    console.log(`env is ${env}.`);
     switch (env) {
       case 'prod':
       case 'gh-pages':
-        console.log('prod or gh-pages condition met.');
         // Remove 'index' from the end of any hrefs.
         siteLink.href = siteLink.href.substring(0, siteLink.href.length - 5);
         break;
       case 'local':
-        console.log('local condition met.');
         siteLink.href += '.html';
     }
-  } else {
-    console.log('modifyHref did nothing.');
   }
 }
 
