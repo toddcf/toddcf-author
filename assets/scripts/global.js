f031e12774c9544350d4d787b2d1b62de67b8ad3
Todd Croak-Falen <toddcf@gmail.com>
2023-07-17 23:29:03 -0700

    Update all image files

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -568,10 +568,7 @@ const buildAlbumCard = (artistName, album) => {
   console.log(`Card being built for ${album.title}`);
   // Create image SRC:
   const imgSrc = setRelativePath(`assets/images/music/${kebabCase(artistName)}/${kebabCase(album)}.jpg`);
-  // Fix all my file names to follow this naming convention. RESUME: NEW ORDER
-  // Resize every file to 300px x 300px.
-  // Convert everything to .jpg that isn't already.
-  // Minimize them all.
+  // Minimize all images.
   // Create the Album Card to store everything in:
   const albumCard = `<div class="row">
       <div class="album-card">
