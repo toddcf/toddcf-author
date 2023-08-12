10b630d45b5a5d67c4681182a0a03b62896bd4fe
Todd Croak-Falen <toddcf@gmail.com>
2023-08-11 18:52:47 -0700

    Abstract Footer logic into its own file

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -189,33 +189,8 @@ if (pageLevel1 === 'titles' && !!pageLevel2) {
 // First build the data layer, then load the Nav logic, which will use those values to dynamically build the Nav:
 
 
-// Footer
-const thisYear = new Date().getFullYear();
+// Load footer.js
 
-// For the footer, maybe I should hardcode an empty footer element into each page's HTML.  Then I can give it a class if I want the full-blown dynamic footer built and inserted, and leave the class off if I just want the copyright date inserted.  That would be a little more dynamic than hardcoding a check for the homepage -- if I ever add another page later where I don't want the full-blown footer, it will be built automatically.
-if (!!pageLevel1 && pageLevel1 !== 'home') {
-  const footerEl = document.createElement('footer');
-  footerEl.classList.add('footer');
-  footerEl.innerHTML = `<section class="footer__section">
-  <div class="footer__icon_flexbox">
-    <a href="https://www.facebook.com/toddcf.writer/" target="_blank"><img src="${window.global.setRelativePath('assets/images/icons/facebook/facebook-20', '.png')}" alt="Facebook Author Page" class="footer__icon"></a>
-    <a href="https://www.amazon.com/Todd-Croak-Falen/e/B003A1UF3I/ref=sr_ntt_srch_lnk_1?qid=1499390370&sr=8-1" target="_blank"><img src="${window.global.setRelativePath('assets/images/icons/amazon/amazon_a_ding', '.png')}" alt="Amazon Author Page" class="footer__icon"></a>
-    <a href="https://www.goodreads.com/toddcf" target="_blank"><img src="${window.global.setRelativePath('assets/images/icons/goodreads/goodreads', '.png')}" alt="Goodreads Author Page" class="footer__icon"></a>
-  </div>
-  </section>
-
-  <section class="footer__section">
-    <p class="copyright font_size_body footer_p">We are a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for us to earn fees by linking to Amazon.com and affiliated sites.</p>
-    <p class="font_size_body footer_p">Website by <a href="https://www.tcf-webdesign.com" target="_blank" class="footer__text-link"> TCF Web Design</a></p>
-    <p class="copyright font_size_body footer_p">Copyright &copy; 2008 ${(!!thisYear) ? `&ndash; ${thisYear} ` : ''}Todd Croak-Falen</p>
-  </section>`;
-  document.body.appendChild(footerEl);
-} else if (!!thisYear) {
-  const currentYearHTML = document.querySelector('.currentYear');
-  if (!!currentYearHTML) {
-    currentYearHTML.innerHTML = `&ndash; ${thisYear} `; // Homepage
-  }
-}
 
 // Add CSS Links:
 // REFACTOR TO USE WINDOW.GLOBAL.ELEMENTBUILDER, AND CONSOLIDATE THIS WITH THE FAVICON FUNCTION.
