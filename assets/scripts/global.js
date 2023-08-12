9fbdc69175063ab41ff77b1c6ce0c253f8cd9d5c
Todd Croak-Falen <toddcf@gmail.com>
2023-08-11 20:49:33 -0700

    Move global.js to head, and create a separate loadModules.js script for the bottom of each page

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -247,23 +247,6 @@ switch (window.digitalData.page.level1) {
     break;
 }
 
-// ALL THE REST OF THESE MAY NEED TO INCORPORATE DOCUMENT.ONLOAD IF APPENDING THEM TO THE BODY IS NOT ENOUGH.
-window.global.elementBuilder('assets/scripts/nav', 'relative', 'js', 'body', true);
-
-if (window.digitalData.page.level1 === 'titles') {
-  window.global.elementBuilder('assets/scripts/titles', 'relative', 'js', 'body', true); // Need additional logic to drill a level deeper if this is a series.  Also, Page Level 1 alone may or may not be a good idea, given that this will load the Titles script on Music pages, as well.
-}
-
-// First build the data layer, then load the Nav logic, which will use those values to dynamically build the Nav:
-
-switch (window.digitalData.page.category) {
-  case 'music':
-    window.global.elementBuilder('assets/scripts/music', 'relative', 'js', 'body', true); // Maybe this process can be even more programmatic, in that every single directory level has an asset, and we just load whatever those files are (with validations that prevent them from attempting to load if they don't exist).
-    break;
-}
-
-window.global.elementBuilder('assets/scripts/footer', 'relative', 'js', 'body', true);
-
 // Create all favicons last, since they are less important than the page content.  And see if there is a way to 
 const createFaviconTag = (createEl, elType, rel, sizes, hrefFilename, color, tagName, content) => {
   const faviconTag = document.createElement(createEl);
