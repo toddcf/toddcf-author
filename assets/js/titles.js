46848ba95695918d918852e7ee77b5c4a62bf4d0
Todd Croak-Falen <toddcf@gmail.com>
2024-03-10 19:16:27 -0700

    Refactor testimonials into dynamic build

--- assets/js/titles.js
+++ assets/js/titles.js
@@ -155,6 +155,60 @@ digitalData.titles = {
 }
 
 window.globalControl.titlePageBuilder = {
+  testimonials: (testimonialsArr) => {
+    const testimonialsFlexbox = document.querySelector('.testimonials-flexbox');
+    if (!!testimonialsFlexbox) {
+      if (Array.isArray(testimonialsArr)) {
+        testimonialsArr.forEach((testimonial, i) => {
+          // First create the quote:
+          const testimonialParagraphs = testimonial.text;
+          let testimonialQuote = '';
+          if (Array.isArray(testimonialParagraphs)) {
+            testimonialParagraphs.forEach(testimonialParagraph => {
+              testimonialQuote += `<p class="body_p font_size_body">&ldquo;${testimonialParagraph}${(i === testimonialParagraphs.length + 1) ? '&rdquo;' : ''}</p>`;
+            });
+          }
+          
+          // Then create the card:
+          const testimonialsFlexboxItem = document.createElement('div');
+          testimonialsFlexboxItem.classList.add('testimonials-flexbox__item');
+          testimonialsFlexboxItem.innerHTML = `<div class="testimonials-card testimonials-card_${i+1}"><blockquote class="testimonials-card__quote" cite="${testimonial.citeLink}">${testimonialQuote}</blockquote><p class="body_p font_size_body testimonials-card__attribution">&ndash;&nbsp;via <a class="testimonials-card__attribution-link" href="${testimonial.citeLink}" target="_blank">${testimonial.platform}</a></p></div>`;
+          
+          // Then append the card:
+          testimonialsFlexbox.appendChild(testimonialsFlexboxItem);
+
+
+
+
+          // <div class="testimonials-flexbox__item">
+					// 	<div class="testimonials-card testimonials-card_1">
+					// 		<blockquote class="testimonials-card__quote" cite="https://www.amazon.com/gp/customer-reviews/R3UW2XCEKRMIP5/ref=cm_cr_dp_d_rvw_ttl?ie=UTF8&ASIN=B003AKZBII">
+					// 			<p class="body_p font_size_body">&ldquo;An earnest and profound book about a young man&rsquo;s coming of age. The level of detail and insight is uncanny; at times, it&rsquo;s almost like reading pages torn from a person&rsquo;s diary.&rdquo;</p>
+					// 		</blockquote>
+					// 		<p class="body_p font_size_body testimonials-card__attribution">&ndash;&nbsp;via <a class="testimonials-card__attribution-link" href="https://www.amazon.com/gp/customer-reviews/R3UW2XCEKRMIP5/ref=cm_cr_dp_d_rvw_ttl?ie=UTF8&ASIN=B003AKZBII" target="_blank">Amazon</a></p>
+					// 	</div>
+					// </div>
+
+
+
+
+          // {
+          //   author: {
+          //     firstName: 'Jon',
+          //     lastName: 'F.',
+          //   },
+          //   citeLink: 'https://www.amazon.com/gp/customer-reviews/R3UW2XCEKRMIP5/ref=cm_cr_dp_d_rvw_ttl?ie=UTF8&ASIN=B003AKZBII',
+          //   platform: 'Amazon',
+          //   text: ['An earnest and profound book about a young man&rsquo;s coming of age.  The level of detail and insight is uncanny; at times, it&rsquo;s almost like reading pages torn from a person&rsquo;s diary.',],
+          // },
+        });
+      } else {
+        console.warn('testimonialsArr was not an array.');
+      }
+    } else {
+      console.warn('No Testimonials Flexbox found on page.');
+    }
+  },
   synopsis: () => {
 
   },
@@ -162,21 +216,20 @@ window.globalControl.titlePageBuilder = {
     console.log('window.globalControl.titlePageBuilder.artwork() invoked.');
     const titleDetails = document.querySelector('.title-details');
     const figure = document.createElement('figure');
-    figure.classList.add('front-cover', 'animated-3', 'fadeInUp');
-    figure.innerHTML = `<img src="../../assets/img/titles/the-druggist/front-1563x2400.jpg" alt="The Druggist">`; // REFACTOR TO USE TAGBUILDER -- AND UPDATE TAGBUILDER TO HANDLE IMG TAGS.
+    // figure.classList.add('front-cover', 'animated-3', 'fadeInUp');
+    // figure.innerHTML = `<img src="../../assets/img/titles/the-druggist/front-1563x2400.jpg" alt="The Druggist">`; // REFACTOR TO USE TAGBUILDER -- AND UPDATE TAGBUILDER TO HANDLE IMG TAGS.
     titleDetails.appendChild(figure);
   },
   init: () => {
     console.log('window.globalControl.titlePageBuilder.init() invoked.');
     if (window.digitalData?.page?.category === 'specific-title') {
-      // Before triggering titlePageBuilder, next step should probably be to populate the data layer accordingly.  BUT FIRST, GET DIGITALDATABUILDER WORKING.
-      // Once data layer is populated, begin building the DOM:
-      window.globalControl.titlePageBuilder.artwork();
+      // window.globalControl.titlePageBuilder.artwork(); // HARDCODED IN DOM FOR NOW.
+      window.globalControl.titlePageBuilder.testimonials(window.digitalData.titles[window.digitalData.page.level2].testimonials); // Note that this is still only flat logic for standalone projects. Series will require additional logic.
     }
   },
 }
 
-// window.globalControl.titlePageBuilder.init(); // DON'T INITIALIZE FOR NOW -- WE ARE HARDCODING THE TITLE PAGES AT THIS TIME.
+window.globalControl.titlePageBuilder.init();
 
 // Waypoints for project testimonials:
 $('.testimonials-card_1').waypoint( function( direction ) {
