de7136a13f22df53e9f0d806e7e823bb466ad689
Todd Croak-Falen <toddcf@gmail.com>
2024-03-29 20:29:38 -0700

    Abstract titles.js into two files

---
+++ assets/js/titles/digitalData.js
@@ -0,0 +1,328 @@
+// Add all title info to data layer:
+window.digitalData.titles = {
+  'camp-paradise': {},
+  'catch-up-to-myself': {
+    title: 'Catch Up To Myself',
+    category: 'fiction',
+    format: {
+      hardcover: {
+        live: false,
+        saleLink: '',
+      },
+      kindle: {
+        live: true,
+        saleLink: 'https://amzn.to/3saCirE',
+      },
+      paperback: {
+        live: false,
+        saleLink: '',
+      },
+    },
+    genres: [
+      'Coming of Age',
+      'Relationships',
+      'Heartbreak',
+      'Young Love',
+      'College',
+      'Social Anxiety',
+      'Depression',
+    ],
+    media: 'novel',
+    synopsis: {
+      logline: '',
+      short: '',
+      full: [],
+    },
+    testimonials: [
+      {
+        author: {
+          firstName: 'Jon',
+          lastName: 'Frechette',
+        },
+        citeLink: 'https://amzn.to/4a393aT',
+        platform: 'Amazon',
+        text: ['An earnest and profound book about a young man&rsquo;s coming of age.  The level of detail and insight is uncanny; at times, it&rsquo;s almost like reading pages torn from a person&rsquo;s diary.',],
+      },
+      {
+        author: {
+          firstName: 'Todd',
+          lastName: 'Luoto',
+        },
+        citeLink: 'https://amzn.to/4a4aIgr',
+        platform: 'Amazon',
+        text: ['This compelling and moving read should appeal to both those who are coming of age and &mdash; probably even more so &mdash; to those brave enough to journey back to such a time.  Highly recommend it.',],
+      },
+      {
+        author: {
+          firstName: 'Andrew',
+          lastName: 'Eastwick',
+        },
+        citeLink: 'https://amzn.to/48NvgZq',
+        platform: 'Amazon',
+        text: ['Prepare for a moving, sometimes funny, sometimes painful, and always authentic journey through the emotional journey of early adulthood.',],
+      },
+      {
+        author: {
+          firstName: 'Mike',
+          lastName: 'Croak',
+        },
+        platform: 'email',
+        text: ['I love this book! It&rsquo;s very introspective, and .&nbsp;.&nbsp;. has a good mix of inner thoughts and action.',],
+      },
+      {
+        author: {
+          firstName: 'Michael',
+          lastName: 'Melilli',
+        },
+        citeLink: 'https://www.goodreads.com/book/show/8008792-catch-up-to-myself',
+        platform: 'Goodreads',
+        text: ['I think the strongest part of this book is the sense of realism that seems to flow throughout the story.', 'I really felt like I was reading an unbelievably honest look at what life at the end of, and after, college is like for your average American student, and you did a remarkable job of portraying the sense of loss that many go through today.',],
+      },
+      {
+        author: {
+          firstName: 'Tiffany',
+          lastName: '',
+        },
+        citeLink: 'https://amzn.to/4ceM2U8',
+        platform: 'Amazon',
+        text: ['I began to feel all the emotions Lem experienced. I was on the edge of my set a few times and I enjoyed the whole book.',],
+      },
+      {
+        author: {
+          firstName: 'T.J.',
+          lastName: 'Rudolph',
+        },
+        citeLink: 'https://www.goodreads.com/book/show/8008792-catch-up-to-myself',
+        platform: 'Goodreads',
+        text: ['This is a story of a college guy trying to find himself while surrounded by peers who think they&rsquo;ve got it all figured out (they don&rsquo;t), and also trying to navigate his own emotional demons.', 'A great first book from this author, and if he has anything else out, I will definitely give it a try!',],
+      },
+      {
+        author: {
+          firstName: 'Arwen',
+          lastName: 'Slosse',
+        },
+        platform: 'email',
+        text: ['I read the novel in four days, which is incredible considering that fact that I&rsquo;m so busy. Just couldn&rsquo;t stop reading it. I ended up taking it everywhere I went (bus, on the stepper, and in class).', 'Now that I finished reading it, I feel sad. I started liking the character Lem (his thinking, feeling, and behaving) very much.',],
+      },
+      {
+        author: {
+          firstName: 'Lance',
+          lastName: 'Hammond',
+        },
+        citeLink: 'https://amzn.to/4a6LdeW',
+        platform: 'Amazon',
+        text: ['A very enjoyable read. The lead character, Lem, is probably going to resonate with all readers. The struggle to resolve, in his own mind, the important relationships in his life will undoubtably ring true for the reader of nearly any age.',],
+      },
+    ],
+  },
+  'cultivation-series': [],
+  'epic-series': [],
+  'operation-paintball': {},
+  'rushing-falls': {
+    title: 'Rushing Falls',
+    category: 'fiction',
+    format: {
+      hardcover: {
+        live: false,
+        saleLink: '',
+      },
+      kindle: {
+        live: false,
+        saleLink: '',
+      },
+      paperback: {
+        live: false,
+        saleLink: '',
+      },
+    },
+    genres: [
+      'Horror',
+    ],
+    media: 'short-story',
+    synopsis: {
+      logline: '',
+      short: '',
+      full: [],
+    },
+    testimonials: [],
+  },
+  'sobriquet-series': [
+    {
+      'sobriquet-the-forging-of-a-special-liaison': {
+        title: 'Sobriquet: The Forging of a Special Liaison',
+        category: 'fiction',
+        format: {
+          hardcover: {
+            live: false,
+            saleLink: '',
+          },
+          kindle: {
+            live: false,
+            saleLink: '',
+          },
+          paperback: {
+            live: false,
+            saleLink: '',
+          },
+        },
+        genres: [
+          'Action',
+          'Relationships',
+          'Self-Improvement',
+        ],
+        media: 'novel',
+        synopsis: {
+          logline: '',
+          short: ``,
+          full: ``,
+        },
+        testimonials: [
+          {
+            author: '',
+            link: '',
+            platform: '',
+            text: ``,
+          },
+        ],
+      },
+    },
+    {
+      'sobriquet-red-team': {},
+    },
+  ],
+  'the-druggist': {
+    title: 'The Druggist',
+    category: 'fiction',
+    format: {
+      hardcover: {
+        live: false,
+        saleLink: '',
+      },
+      kindle: {
+        live: true,
+        saleLink: 'https://amzn.to/3x9U1SI',
+      },
+      paperback: {
+        live: false,
+        saleLink: '',
+      },
+    },
+    genres: [
+      'Horror',
+      'Halloween',
+    ],
+    media: 'short-story',
+    synopsis: {
+      logline: '',
+      short: [],
+      full: [],
+    },
+    testimonials: [
+      {
+        author: {
+          firstName: 'Jcat',
+          lastName: '',
+        },
+        citeLink: 'https://amzn.to/4cnI0ZL',
+        platform: 'Amazon',
+        text: ['Not for the queasy, nor the faint of heart. Perfect for a quick Halloween read.',],
+      },
+      {
+        author: {
+          firstName: 'Dominic',
+          lastName: 'Wahl-Stephens',
+        },
+        citeLink: 'https://amzn.to/3Vn6AnE',
+        platform: 'Amazon',
+        text: ['The story reads like a nightmare that is very alive and frightening. The last scene is masterful; very Poe-esque.',],
+      },
+      {
+        author: {
+          firstName: 'Lance',
+          lastName: 'Hammond',
+        },
+        citeLink: 'https://amzn.to/43AVe1l',
+        platform: 'Amazon',
+        text: ['<cite>The Druggist</cite> is a sharp-edged, crisp story that is very dark. Another great effort by Croak-Falen. In this tale of horror, he develops a great pace and finishes strong.',],
+      },
+      {
+        author: {
+          firstName: 'C',
+          lastName: 'H',
+        },
+        citeLink: 'https://amzn.to/3PzbYQJ',
+        platform: 'Amazon',
+        text: [
+          'Just look at the cover of this book &mdash; does that say nightmares, or what? And I was crazy enough to read it right before bed with my Kindle being the only light in the room!',
+          'I highly recommend this to anyone who likes creepy, rather dark, scary, nightmarish stories! This is a story that will play with your mind.',
+        ],
+      },
+      {
+        author: {
+          firstName: 'TX_Neener',
+          lastName: '',
+        },
+        citeLink: 'https://amzn.to/3Vuxz0K',
+        platform: 'Amazon',
+        text: [
+          'The short time I spent reading this wild ride of a story is nothing compared to the time I spent contemplating its contents and the fear, the chill, the overall thrilling sensation that I was left with when it was finished.',
+          `Definitely worth the read, and now I&rsquo;m intrigued enough by Croak-Falen&rsquo;s writing to check out his book, <cite><a class="testimonials-card__quote-link" data-link="internal" href="${window.digitalData.page.pathToRoot}/titles/catch-up-to-myself/index">Catch Up To Myself</a></cite>.`,
+        ],
+      },
+      {
+        author: {
+          firstName: 'Jon',
+          lastName: 'Frechette',
+        },
+        citeLink: 'https://amzn.to/4ammvql',
+        platform: 'Amazon',
+        text: ['This is one of the most effective Halloween-themed short stories I&rsquo;ve ever read. Not only does it have an engaging (and unsettling) plot, but it also features a truly novel use of the first-person that makes the writing come nightmarishly to life.', 'The ending &mdash; which I wouldn&rsquo;t dare spoil &mdash; really kicked me in the gut. Highly recommended for anyone in the mood for a macabre page-turner.',],
+      },
+      {
+        author: {
+          firstName: 'Haunted',
+          lastName: 'Reader',
+        },
+        citeLink: 'https://amzn.to/3VxrvEv',
+        platform: 'Amazon',
+        text: ['Very vividly described, all senses are evoked, and the reader&rsquo;s heart strings are repeatedly tugged. A surprising kicker ending, followed by a startling denouement create a memorable, thought-provoking story.',],
+      },
+      {
+        author: {
+          firstName: 'Daddy',
+          lastName: '',
+        },
+        citeLink: 'https://amzn.to/3PzgYVz',
+        platform: 'Amazon',
+        text: ['This rather disturbing tale has left me literally speechless. Maybe I&rsquo;m weird, but I really liked it!',],
+      },
+      {
+        author: {
+          firstName: 'Todd',
+          lastName: 'Luoto',
+        },
+        citeLink: 'https://amzn.to/4cr3WDm',
+        platform: 'Amazon',
+        text: [`After reading (and very much enjoying) his novel <cite><a class="testimonials-card__quote-link" data-link="internal" href="${window.digitalData.page.pathToRoot}/titles/catch-up-to-myself/index">Catch Up To Myself</a></cite>, I wanted to check out this Halloween-themed short story by author Todd Croak-Falen. It did not disappoint! Excellent writing; a spooky, spooky narrative.`, 'Might keep you up for a few hours during the night you decide to get into it ... but for those like to be scared, consider it well worth it.',],
+      },
+      {
+        author: {
+          firstName: 'Andrew',
+          lastName: 'Eastwick',
+        },
+        citeLink: 'https://amzn.to/4ajy8yg',
+        platform: 'Amazon',
+        text: [`This is a genuinely spooky story that will pull you right in! It&rsquo;s bizarre and surreal and completely avoids cliche. A totally different style from Todd Croak-Falen&rsquo;s novel <cite><a class="testimonials-card__quote-link" data-link="internal" href="${window.digitalData.page.pathToRoot}/titles/catch-up-to-myself/index">Catch Up To Myself</a></cite>, but every bit as good.`,],
+      },
+      {
+        author: {
+          firstName: 'Maria2101',
+          lastName: '',
+        },
+        citeLink: 'https://amzn.to/43tkvdI',
+        platform: 'Amazon',
+        text: [`I read Todd Croak-Falen&rsquo;s novel <cite><a class="testimonials-card__quote-link" data-link="internal" href="${window.digitalData.page.pathToRoot}/titles/catch-up-to-myself/index">Catch Up To Myself</a></cite>, and enjoyed it a lot, so when I heard that he published a short story, I was really excited. <cite>The Druggist</cite> is a fantastic and creepy story that haunted my spirits for a few days.`,],
+      },
+    ],
+  },
+}
---
+++ assets/js/titles/logic.js
@@ -0,0 +1,131 @@
+window.globalControl.titlePageBuilder = {
+  testimonials: (testimonialsArr) => {
+    const testimonialsFlexbox = document.querySelector('.testimonials-flexbox');
+    if (!!testimonialsFlexbox) {
+      if (Array.isArray(testimonialsArr)) {
+        testimonialsArr.forEach((testimonial, testimonialNumber) => {
+          // First create the quote:
+          const testimonialParagraphs = testimonial.text;
+          let testimonialQuote = '';
+          if (Array.isArray(testimonialParagraphs)) {
+            testimonialParagraphs.forEach((testimonialParagraph, paragraphNumber) => {
+              testimonialQuote += `<p class="body_p font_size_body">&ldquo;${testimonialParagraph}${(paragraphNumber === testimonialParagraphs.length - 1) ? '&rdquo;' : ''}</p>`;
+            });
+          }
+
+          // Then create the attribution:
+          let testimonialAttribution;
+          if (!!testimonial.citeLink) {
+            testimonialAttribution = `<a class="testimonials-card__attribution-link" href="${testimonial.citeLink}" target="_blank"' : ''}>${testimonial.platform}</a>`;
+          } else {
+            testimonialAttribution = testimonial.platform;
+          }
+          
+          // Then create the card:
+          let animationDelay = testimonialNumber * 400; // ms
+          const testimonialsFlexboxItem = document.createElement('div');
+          testimonialsFlexboxItem.classList.add('testimonials-flexbox__item');
+          const cardNumber = testimonialNumber + 1;
+          testimonialsFlexboxItem.innerHTML = `<div class="testimonials-card testimonials-card_${cardNumber}" style="-webkit-animation-delay: ${animationDelay}ms;animation-delay: ${animationDelay}ms;"><blockquote class="testimonials-card__quote" cite="${testimonial.citeLink}">${testimonialQuote}</blockquote><p class="body_p font_size_body testimonials-card__attribution">&ndash;&nbsp;via ${testimonialAttribution}</p></div>`;
+          
+          // Then append the card:
+          testimonialsFlexbox.appendChild(testimonialsFlexboxItem);
+        });
+      } else {
+        console.warn('testimonialsArr was not an array.');
+      }
+    } else {
+      console.warn('No Testimonials Flexbox found on page.');
+    }
+  },
+  // Kind of deciding this is not worth it. It would work, but what is the advantage? I think it will only ever be on this one page, so might as well hardcode it.
+  // synopsis: (titleKebab) => {
+  //   let fullSynopsisArr;
+  //   if (typeof titleKebab === 'string') {
+  //     fullSynopsisArr = window.digitalData.titles[titleKebab].synopsis.full;
+  //     console.log('fullSynopsisArr:', fullSynopsisArr);
+  //     // Probably use Object.keys(obj);
+  //   } else {
+  //     console.warn('No valid title passed into titlePageBuilder.synopsis.');
+  //   }
+  // },
+  cta: (titleKebab) => {
+    const ctaFlexboxes = document.querySelectorAll('.cta-flexbox');
+    ctaFlexboxes.forEach(ctaFlexbox => {
+      ctaFlexbox.innerHTML = `<div class="cta-flexbox__item">
+        <a class="button" href="${window.digitalData.titles[titleKebab].format.kindle.saleLink}" target="_blank">
+          <p class="button_primary">Buy on Amazon</p>
+        </a>
+      </div>
+
+      <div class="cta-flexbox__item">
+        <a class="button" data-link="internal" href="${window.digitalData.page.pathToRoot}/bonus-content/registration">
+          <p class="button_secondary">Bonus Content</p>
+        </a>
+      </div>
+
+      <div class="cta-flexbox__item">
+        <a class="button" data-link="internal" href="music">
+          <p class="button_secondary">The Music of <cite>${window.digitalData.titles[titleKebab].title}</cite></p>
+        </a>
+      </div>`;
+    });
+    window.globalControl.internalLinkLogic();
+  },
+  artwork: (titleKebab) => {
+    // Background Art:
+    const backgroundArt = document.querySelector('.synopsis-section');
+    let backgroundURL;
+    if (!!backgroundArt) {
+      backgroundURL = `${window.digitalData.page.pathToRoot}/assets/img/titles/${titleKebab}/wide-no-text.jpg`;
+      backgroundRGBA = 'rgba(0, 0, 0, 0.7)';
+      backgroundArt.setAttribute('style', `background-image: -webkit-gradient(linear, left top, left bottom, from(${backgroundRGBA}), to(${backgroundRGBA})) , url(${backgroundURL});
+      background-image: -webkit-linear-gradient(${backgroundRGBA}, ${backgroundRGBA}) , url(${backgroundURL});
+      background-image: -o-linear-gradient(${backgroundRGBA}, ${backgroundRGBA}) , url(${backgroundURL});
+      background-image: linear-gradient(${backgroundRGBA}, ${backgroundRGBA}) , url(${backgroundURL});`);
+    } else {
+      console.error('.synopsis-section not found; unable to set background art.');
+    }
+    // Cover Art:
+    const synopsisCoverArtImg = document.querySelector('.synopsis-cover-art__img');
+    synopsisCoverArtImg.src = `${window.digitalData.page.pathToRoot}/assets/img/titles/${titleKebab}/front.jpg`;
+    synopsisCoverArtImg.alt = `${window.digitalData.titles[titleKebab].title} ${synopsisCoverArtImg.alt}`;
+  },
+  init: () => {
+    let titleKebab;
+    const pageLevels = window.digitalData.page.levels;
+    const finalPageLevel = pageLevels[pageLevels.length - 1];
+    switch (finalPageLevel?.category) {
+      case 'specific-title':
+        titleKebab = finalPageLevel.id;
+        break;
+      // case 'series-hub': // The hub will have to take all the titles. Then the individual title case will be 'specific-title' just like above, so figure that out.
+      //   titleKebab = window.digitalData.page.level3;
+      //   break;
+    }
+    if (typeof titleKebab === 'string') {
+      window.globalControl.titlePageBuilder.artwork(titleKebab);
+      // window.globalControl.titlePageBuilder.synopsis(titleKebab);
+      window.globalControl.titlePageBuilder.cta(titleKebab);
+      window.globalControl.titlePageBuilder.testimonials(window.digitalData.titles[titleKebab].testimonials);
+    }
+  },
+}
+
+window.globalControl.titlePageBuilder.init();
+
+// Waypoints for Testimonials:
+if (!!document.querySelector('.testimonials-card')) {
+  $('.testimonials-card_1').waypoint( function( direction ) {
+    const testimonialsCards = document.querySelectorAll('.testimonials-card');
+    if (testimonialsCards.length > 0) {
+      testimonialsCards.forEach(testimonialsCard => {
+        testimonialsCard.classList.add('testimonials-card_fade-in');
+      });
+    }
+  }, {
+    offset: '75%',
+  });
+
+  window.globalControl.internalLinkLogic(); // Not sure if this should go inside or outside of this condition.
+}
