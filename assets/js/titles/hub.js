1d5ffa5abed9e5f7c3625ac0e166a728294c1a8d
Todd Croak-Falen <toddcf@gmail.com>
2024-03-30 18:54:04 -0700

    Fix buildCards syntax

--- assets/js/titles/hub.js
+++ assets/js/titles/hub.js
@@ -1,14 +1,15 @@
 window.globalControl.titlesHubBuilder = {
   buildCards: (hubTitles) => {
     const titlesHubFlexbox = document.querySelector('.titles-hub-flexbox');
+    console.log(titlesHubFlexbox);
     if (!!titlesHubFlexbox) {
       hubTitles.forEach(hubTitle => {
-        const titleHubCard = `<div class="titles-hub-flexbox__item">
-          <div class="title-card">
-            <img class="title-card__artwork" src="${window.digitalData.page.pathToRoot}/assets/img/titles/${hubTitle.id}/front.jpg" alt="${hubTitle.title} cover art">
-            <cite><h2 class="title-card__title font_size_1">${hubTitle.title}</h2></cite>
-            <h3 class="title-card__media font_size_body">- ${hubTitle.media.name} -</h3>
-          </div>
+        const titleHubCard = document.createElement('div');
+        titleHubCard.classList.add('titles-hub-flexbox__item');
+        titleHubCard.innerHTML = `<div class="title-card">
+          <img class="title-card__artwork" src="${window.digitalData.page.pathToRoot}/assets/img/titles/${hubTitle.id}/front.jpg" alt="${hubTitle.title} cover art">
+          <cite><h2 class="title-card__title font_size_1">${hubTitle.title}</h2></cite>
+          <h3 class="title-card__media font_size_body">- ${hubTitle.media.name} -</h3>
         </div>`;
         titlesHubFlexbox.appendChild(titleHubCard);
       });
