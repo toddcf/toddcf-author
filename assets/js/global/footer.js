824de04da9cad13bae6b25f32107d471d670ff0d
Todd Croak-Falen <toddcf@gmail.com>
2024-04-06 19:25:40 -0700

    Add data-link=external, and rename footer__p

--- assets/js/global/footer.js
+++ assets/js/global/footer.js
@@ -6,16 +6,16 @@ const footerEl = document.querySelector('.footer');
 if (!!footerEl) {
   footerEl.innerHTML = `<section class="footer__section">
   <div class="footer__icon-flexbox">
-    <a href="https://www.facebook.com/toddcf.writer/" target="_blank"><img src="${window.globalControl.prependRoot('assets/img/icons/facebook/facebook-20.png')}" alt="Facebook Author Page" class="footer__icon"></a>
-    <a href="https://www.amazon.com/Todd-Croak-Falen/e/B003A1UF3I/ref=sr_ntt_srch_lnk_1?qid=1499390370&sr=8-1" target="_blank"><img src="${window.globalControl.prependRoot('assets/img/icons/amazon/amazon_a_ding.png')}" alt="Amazon Author Page" class="footer__icon"></a>
-    <a href="https://www.goodreads.com/toddcf" target="_blank"><img src="${window.globalControl.prependRoot('assets/img/icons/goodreads/goodreads.png')}" alt="Goodreads Author Page" class="footer__icon"></a>
+    <a data-link="external" href="https://www.facebook.com/toddcf.writer/" target="_blank"><img src="${window.globalControl.prependRoot('assets/img/icons/facebook/facebook-20.png')}" alt="Facebook Author Page" class="footer__icon"></a>
+    <a data-link="external" href="https://www.amazon.com/Todd-Croak-Falen/e/B003A1UF3I/ref=sr_ntt_srch_lnk_1?qid=1499390370&sr=8-1" target="_blank"><img src="${window.globalControl.prependRoot('assets/img/icons/amazon/amazon_a_ding.png')}" alt="Amazon Author Page" class="footer__icon"></a>
+    <a data-link="external" href="https://www.goodreads.com/toddcf" target="_blank"><img src="${window.globalControl.prependRoot('assets/img/icons/goodreads/goodreads.png')}" alt="Goodreads Author Page" class="footer__icon"></a>
   </div>
   </section>
 
   <section class="footer__section">
-    <p class="copyright font_size_body footer_p">We are a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for us to earn fees by linking to Amazon.com and affiliated sites.</p>
-    <p class="font_size_body footer_p">Website by <a href="https://tcf-web-development.com/" target="_blank" class="footer__text-link"> TCF Web Development</a></p>
-    <p class="copyright font_size_body footer_p">Copyright &copy; 2008 ${(!!thisYear) ? `&ndash; ${thisYear} ` : ''}Todd Croak-Falen</p>
+    <p class="copyright font_size_body footer__p">We are a participant in the Amazon Services LLC Associates Program, an affiliate advertising program designed to provide a means for us to earn fees by linking to Amazon.com and affiliated sites.</p>
+    <p class="font_size_body footer__p">Website by <a data-link="external" href="https://tcf-web-development.com/" target="_blank" class="footer__text-link"> TCF Web Development</a></p>
+    <p class="copyright font_size_body footer__p">Copyright &copy; 2008 ${(!!thisYear) ? `&ndash; ${thisYear} ` : ''}Todd Croak-Falen</p>
   </section>`;
   document.body.appendChild(footerEl);
 }
