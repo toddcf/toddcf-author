c1987db2756cc5b597b7ec666beb8c6427dce461
Todd Croak-Falen <toddcf@gmail.com>
2024-04-12 17:13:58 -0700

    Fix syntax that was throwing error

--- assets/js/music/logic.js
+++ assets/js/music/logic.js
@@ -20,58 +20,16 @@ window.globalControl.musicPageBuilder = {
   },
   buildAlbumCard1: (artistName, album) => {
     // THIS IS THE LEGACY VERSION
-    const imgSrc = window.globalControl.prependRoot(`assets/img/music/${window.globalControl.kebabCase(artistName)}/${window.globalControl.kebabCase(album.title)}.jpg`);
-    // NOTE: Minimize all images.
-    // Build out all the tracks first, as they will need to be ready to be inserted into the album card at the time the Album Card is created:
-    const trackNumWidth = '1';
-    const trackTitleWidth = '6';
-    const trackNotesWidth = '5';
-    
-    // Create one row of headings:
-    let tracksContainer = `<div class="row"><div class="col col-${trackNumWidth}"><p class="track-number">Track Number</p></div><div class="col col-${trackTitleWidth}"><p class="track-title">Track Title</p></div><div class="col col-${trackNotesWidth}"><p class="track-notes">Notes</p></div></div>`;
-    
-    // Add each applicable track:
-    const tracks = album.tracks;
-    tracks.forEach(track => {
-      if (projectTitle in track.notes) {
-        tracksContainer += `${window.globalControl.musicPageBuilder.buildTrackInfo(track, trackNumWidth, trackTitleWidth, trackNotesWidth)}`;
-      }
-    });
-    
-    // Create the Album Card to store everything in:
-    const albumCard = document.createElement('div');
-    albumCard.classList.add('row'); // Something is slightly wrong with the row/column nesting in these Album Cards, but I'm not sure what yet.
-
     // Only create anchor tags if a link exists:
     const anchorTagOpen = (!!album.saleLink) ? `<a href="${album.saleLink}" target="_blank">` : '';
     const anchorTagClose = (!!album.saleLink) ? '</a>' : '';
-
-    albumCard.innerHTML = `
-      <div class="album-card">
-        <div class="col col-3">
-          <figure class="album-art">
-            ${anchorTagOpen}
-              <img class="image-assets" src="${imgSrc}" alt="${artistName}: ${album.title}">
-            ${anchorTagClose}
-            <figcaption class="album-title">${anchorTagOpen}${album.title}${anchorTagClose}</figcaption>
-          </figure>
-        </div>
-        <div class="col col-9">
-          ${window.globalControl.musicPageBuilder.buildParagraphs(album.notes[projectTitle])}
-        </div>
-        <div class="row">
-          <div class="col-12">
-            ${tracksContainer}
-          </div>
-        </div>
-      </div>`;
 
     // Attach the Album Card to the page:
     if (!!musicCardsContainer) {
       musicCardsContainer.appendChild(albumCard);
     }
   },
-  buildAlbumTracks: () => {
+  buildTrackNotes: () => {
 
   },
   buildArtistNotes: (artistNotesArr) => {
@@ -116,7 +74,7 @@ window.globalControl.musicPageBuilder = {
   buildAlbumCard: (album, artist) => {
     // Before building the HTML for the Album Card itself, build the dynamic HTML that it may or may not contain:
     const albumNotesHTML = window.globalControl.musicPageBuilder.buildAlbumNotes(album.notes[projectTitle]);
-    const albumTracksHTML = window.globalControl.musicPageBuilder.buildAlbumTracks(album.notes[projectTitle].tracks); // I want to pass in the 'tracks' array, but have a feeling this is not quite the right syntax. Also, I think we'll need to filter for just the tracks pertaining to this project first...
+    const albumTracksHTML = window.globalControl.musicPageBuilder.buildTrackNotes(album.notes[projectTitle].tracks); // I want to pass in the 'tracks' array, but have a feeling this is not quite the right syntax. Also, I think we'll need to filter for just the tracks pertaining to this project first...
 
     return `<div class="music-card_album">
       <figure class="music-card__album-artwork_figure">
@@ -131,19 +89,22 @@ window.globalControl.musicPageBuilder = {
         </div>
       </div>`;
   },
-  filterAlbums: (artistAlbums, artist) => {
+  filterAlbums: (artist) => {
     let albumCardsHTML = '';
+    const artistAlbums = artist.albums;
     artistAlbums.forEach(album => {
       if (projectTitle in album.notes) {
-        albumCardsHTML += window.globalControl.musicPageBuilder.buildAlbumCard(album, artist);
+        albumCardsHTML += window.globalControl.musicPageBuilder.buildAlbumCard(album, artist.artist);
       }
     });
     return albumCardsHTML;
   },
   buildArtistCard: (applicableArtists) => {
+    console.log('applicableArtists:', applicableArtists);
     applicableArtists.forEach(artist => {
+      //console.log('artist.notes[projectTitle]:', artist.notes[projectTitle]);
       // Before building the HTML for the Artist Card itself, build the dynamic HTML that it may or may not contain:
-      const albumCardsHTML = window.globalControl.musicPageBuilder.filterAlbums(artist.albums.notes[projectTitle], artist.artist);
+      const albumCardsHTML = window.globalControl.musicPageBuilder.filterAlbums(artist);
       const artistNotesHTML = window.globalControl.musicPageBuilder.buildArtistNotes(artist.notes[projectTitle]);
 
       // *Now* build the HTML for the actual Artist Card, inserting each applicable element:
